a4042942b5d8a9d88576a55aed95e0e3d61afe57
Bubeshnedumaran <79853076+Bubeshnedumaran@users.noreply.github.com>
2025-06-02 10:31:30 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TextEditorComponent } from './text-editor/text-editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TextEditorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
